6a8a9af3f391d3befbaabaed807d6b3b2afe6a0d
Bobson Lin <bobson801104@gmail.com>
2023-12-05 06:13:11 +0800

    feat: Negetive page for audios

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="text-4xl text-coolGray-4">過去的生命中，有哪個負面的聲音一直影響著你？</div>
-  <PrimeTextarea></PrimeTextarea>
+  <div class="flex flex-col items-center mt-36">
+    <div class="typer-line text-5xl text-coolGray-4 anim-typewriter">過去的生命中，有哪個負面的聲音一直影響著你？</div>
+    <PrimeTextarea class="mt-4 w-2/3 b-coolGray b-3" rows="5"></PrimeTextarea>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -8,5 +10,26 @@
 </script>
 
 <style scoped>
+/* Animation */
+.typer-line{
+    position: relative;
+    border-right: 2px solid rgba(255,255,255,.75);
+    white-space: nowrap;
+    overflow: hidden;   
+}
 
+.anim-typewriter{
+  animation: typewriter 4s steps(50) 1s 1 normal both,
+             blinkTextCursor 500ms steps(50) infinite normal;
+}
+
+@keyframes typewriter{
+  from{width: 0;}
+  to{width: 22em;}
+}
+
+@keyframes blinkTextCursor{
+  from{border-right-color: rgba(255,255,255,.75);}
+  to{border-right-color: transparent;}
+}
 </style>
---
+++ pages/neg-[id].vue
@@ -0,0 +1,26 @@
+<template>
+  {{ $route.params.id }}
+  <button @click="play">PLAY</button>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { Howl } from 'howler'
+
+var sound = new Howl({
+  src: ['/assets/audio/1-neg.mp3'],
+  volume: 0.5,
+});
+
+const play = () => {
+  console.log('sound', sound)
+
+  sound.play();
+}
+
+onMounted(() => {
+  play()
+})
+</script>
+
+<style scoped></style>
